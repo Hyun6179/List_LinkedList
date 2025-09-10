691e76770ead67af3317f8568669a1aa42a97764
HyunMin Lim <lhm6179@naver.com>
2025-09-10 16:01:50 +0900

    20250910

---
+++ 20250910/BJ10808.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace _20250910
+{
+    class BJ10808
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine()!;
+            string array = "abcdefghijklmnopqrstuvwxyz";
+            int[] result = new int[26];
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(input);
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (sb[i] == array[j])
+                    {
+                        result[j]++;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write(result[i] + " ");
+            }
+        }
+    }
+}
---
+++ 20250910/BJ2979.cs
@@ -0,0 +1,36 @@
+﻿namespace _20250910
+{
+    class BJ2979
+    {
+        static void Main()
+        {
+
+            // 1대면 5 2대명 3 3대면 1
+            string[] input = Console.ReadLine()!.Split();
+            int cost = 0;
+            int[] array = new int[100];
+
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] time = Console.ReadLine()!.Split();
+                int x = int.Parse(time[0]);
+                int y = int.Parse(time[1]);
+
+                for (int j = x;  j < y; j++)
+                {
+                    array[j]++;
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == 1) cost += array[i] * int.Parse(input[0]);
+                else if (array[i] == 2) cost += array[i] * int.Parse(input[1]);
+                else if (array[i] == 3) cost += array[i] * int.Parse(input[2]);
+            }
+
+            Console.WriteLine(cost);
+        }
+    }
+}
